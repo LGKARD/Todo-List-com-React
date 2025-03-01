a2c29432255e7cf93c08ae9ff2689f868d2942f2
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-03-01 08:10:30 -0300

    Criando função de checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -21,20 +21,31 @@ function App() {
 
   }
 
+  const marcarCompleta = (id: string) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed }
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
   return (
     <>
       <div className='app'>
         <div className='container'>
           <h1>Todo List</h1>
           <div className='input-container'>
-            <input type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)}/>
+            <input type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
             <button onClick={adicionarTarefa}>Adicionar tarefa</button>
           </div>
           <ol>
             {
               todos.map((todo) => (
                 <li key={todo.id}>
-                  <span>{todo.text}</span>
+                  <input type="checkbox" checked={todo.completed} onChange={() => marcarCompleta(todo.id)} />
+                  <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>{todo.text}</span>
                 </li>
               ))
             }
